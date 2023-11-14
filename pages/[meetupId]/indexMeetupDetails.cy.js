a93f34f4707c1d59babb612dcd21c1912ca885a0
Sanaz Dehghannayyeri <sanazdehghannayyeri@Sanazs-MacBook-Pro-2.local>
2023-11-14 11:33:23 -0500

    update build issue

--- pages/[meetupId]/indexMeetupDetails.cy.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-// import MeetupDetails from './index'
-
-// describe('MeetupDetails Component', () => {
-//   it('renders', () => {
-//     // see: https://on.cypress.io/mounting-react
-//     cy.mount(<MeetupDetails />)
-//   })
-// })
